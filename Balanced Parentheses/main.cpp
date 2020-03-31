db5e18cc58626efb82d632e1f0321edc0c6dd196
aboud <62638264+abdalrhmanw@users.noreply.github.com>
2020-04-01 01:55:00 +0300

    Create main.cpp

---
+++ Balanced Parentheses/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+bool arepair(char open,char close)
+{
+    if (open=='('&&close==')')
+        return true;
+    else if (open=='{'&&close=='}')
+        return true;
+    else if (open=='['&&close==']')
+        return true;
+    return false;
+}
+bool arebalanced(string exp)
+{
+    stack <char>s;
+    for (int i=0;i<exp.length();i++)
+    {
+        if (exp[i]=='('||exp[i]=='{'||exp[i]=='[')
+                s.push(exp[i]);
+        else if (exp[i]==')'||exp[i]=='}'||exp[i]==']')
+        {
+            if (s.empty()||!arepair(s.top(),exp[i]))
+                return false;
+            s.pop();
+
+        }
+    }
+    return s.empty();
+}
+int main()
+{
+    string expression;
+    cout<<"enter your expression";
+    cin>>expression;
+    if (arebalanced(expression))
+        cout<<"Balaneced ";
+    else
+        cout<<"Not Balaneced ";
+    return 0;
+}
